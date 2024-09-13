5b8ca1cf61fa9789dd38b4d3fd898dacd48f537c
Ruhani Kapoor <ruhanikapoor04@gmail.com>
2024-09-14 01:08:10 +0530

    styles #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import { fetchWeatherData } from "./services/weatherService";
+import BgVideo from "./video/vid-bg.mp4";
 
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -18,24 +19,50 @@ const App = () => {
   };
   return (
     <div className="app-container">
-      <h1>Climatic</h1>
-      <SearchBar onSearch={handleSearch} />
-      {error && <p className='error-message'>{error}</p>}
-      {weatherData && (
-        <div className="weather-display">
-          {console.log(weatherData)}
-          <h2>Weather in {weatherData.name}</h2>
-          <p>Weather: {weatherData.weather[0].description}</p>
-          <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="weather icon" className="weather-icon"/>
-          <p>Temperature: {weatherData.main.temp} °C</p>
-          <p>Feels Like: {weatherData.main.feels_like} °C</p>
-          <p>Minimum Temperature: {weatherData.main.temp_min} °C</p>
-          <p>Maximum Temperature: {weatherData.main.temp_max} °C</p>
-          <p>Humidity: {weatherData.main.humidity}%</p>
+      <video autoPlay loop muted className="bg-vid">
+        <source src={BgVideo} type="video/mp4" />
+      </video>
+      <div className="content">
+        <div className="header">
+          <h1 className="aboreto">Climatic</h1>
+          <SearchBar onSearch={handleSearch} />
         </div>
-      )}
+        {error && <p className="error-message">{error}</p>}
+        {weatherData && (
+          <div className="cards">
+              <h2>Weather in {weatherData.name}</h2>
+
+            <div className="card-division">
+              <div className="info-card">
+              <div>{weatherData.weather[0].description}</div>
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                alt="weather icon"
+                className="weather-icon"
+              />
+            </div>
+            <div className="info-card weather-temp">
+              <p>Temperature: {weatherData.main.temp} °C</p> <br />
+              <p>Feels Like: {weatherData.main.feels_like} °C</p>
+            </div>
+            </div>
+
+            <div className="card-division">
+              <div className="info-card weather-temp">
+              <p>Minimum Temperature: {weatherData.main.temp_min} °C</p> <br />
+              <p>Maximum Temperature: {weatherData.main.temp_max} °C</p>
+            </div>
+            <div className="info-card">
+              <p>Humidity: {weatherData.main.humidity}%</p>
+            </div>
+            </div>
+            
+            
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
